3bb0f7f2365a71f5d9aa2bbd19c1af173c722e15
Sarah Hong <sarah.y.hong@gmail.com>
2020-04-19 01:12:08 -0700

    Add new math

--- main.cpp
+++ main.cpp
@@ -8,6 +8,14 @@ int main()
 {
 	std::cout << "How do we commit a file?" << endl;
 	std::cout << "Nice, I am getting used to this a bit." << endl;
+	
+	std::cout << "Okay, let's start doing some stuff with variables." << endl;
+	int x = 0;
+	int y = 0;
+
+	x = x + 5; 
+	y = y + x;
+
 	return 0;
 }
 
