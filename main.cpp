e6a957bb8fba170823de9473b558a17f63290d89
Sarah Hong <sarah.y.hong@gmail.com>
2020-04-19 01:23:13 -0700

    Perform string concatenation

--- main.cpp
+++ main.cpp
@@ -6,6 +6,13 @@
 int main()
 {
 	std::cout << "So this is our master branch.";
+	std::cout << "Now we can try to do some string concatenation.";
+
+	std::string s = "Hi";
+	std::string h = "... How are you?";
+
+	std::string sh = s+h;
+
 	return 0;
 }
 
